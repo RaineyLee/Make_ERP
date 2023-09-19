8e3944ca7d8941cb858a85d9eb651f87b87a79b5
Jaehoon Lee <myholyrain@gmail.com>
2023-09-19 14:14:37 +0900

    first commit

--- inventory_location.py
+++ inventory_location.py
@@ -1,6 +1,8 @@
-from openpyxl import *
 import os
 import sys
+import warnings
+
+from openpyxl import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QDate
 from PyQt5 import uic
@@ -19,14 +21,87 @@ def resource_path(relative_path):
 main_window= uic.loadUiType(resource_path("C:\Python Workplace\Make_ERP\windows\inventory_location.ui"))[0] # Window 사용시 ui 주소
 
 #화면을 띄우는데 사용되는 Class 선언
-class WindowClass(QMainWindow, main_window) :
+class WindowClass(QWidget, main_window) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
         self.setWindowTitle("제품위치")
-        # self.slots()
+        self.slots()
 
-        pass
+        self.date_edit.setDate(QDate.currentDate())
+        self.date = self.date_edit.date().toString("yyyyMMdd")   
+
+    def slots(self):
+        self.date_edit.dateChanged.connect(self.set_date)
+        self.btn_open.clicked.connect(self.file_open)
+        self.btn_select.clicked.connect(self.make_data)
+
+        self.btn_close.clicked.connect(self.window_close)
+
+    def set_date(self):
+        self.date = self.date_edit.date().toString("yyyyMMdd")
+
+    def file_open(self):
+        fname = QFileDialog.getOpenFileName(parent=self, caption='Open file', directory='C:\\Users\\mynote\\Downloads')
+
+        if fname[0]:
+            self.text_select_file.setText(fname[0])
+        else:
+            self.text_select_file.setText("")
+            QMessageBox.about(self, 'Warning', '파일을 선택하지 않았습니다.')
+
+    def make_data(self):
+        file_name = self.text_select_file.toPlainText()
+
+        from utils.make_data import Location
+        make_data = Location(file_name)
+
+        _list = make_data.excel_data()
+                
+        self.make_table(len(_list)-1, _list)
+
+    def make_table(self, num, arr_1):
+        self.tbl_info.setRowCount(num)
+        col = self.tbl_info.columnCount()
+
+        for i in range(num):
+            for j in range(self.tbl_info.columnCount()): # 아니면 10개
+                self.tbl_info.setItem(i, j, QTableWidgetItem(arr_1[i][j]))
+
+        # 컨텐츠의 길이에 맞추어 컬럼의 길이를 자동으로 조절
+        ################################################################
+        table = self.tbl_info
+        header = table.horizontalHeader()
+
+        for i in range(col):
+            header.setSectionResizeMode(i, QHeaderView.ResizeToContents)
+        ################################################################
+
+        # 테이블의 길이에 맞추어 컬럼 길이를 균등하게 확장
+            # self.tbl_info.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+    
+    # 테이블 선택범위 삭제
+    def deleteRows(self):
+        indexes = []
+        rows = []
+
+        for idx in self.tbl_info.selectedItems():
+            indexes.append(idx.row())
+
+        for value in indexes:
+            if value not in rows:
+                rows.append(value)
+
+        # 삭제시 오류 방지를 위해 아래서 부터 삭제(리버스 소팅)
+        rows = sorted(rows, reverse=True)
+
+        # 선택행 삭제
+        for rowid in rows:
+            self.tbl_info.removeRow(rowid)
+
+
+    def window_close(self):
+        self.close()
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
